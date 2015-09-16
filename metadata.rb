0221fa70da974ff56534d4940048176f79aba027
Lauri Jesmin <lauri.jesmin@cashongo.co.uk>
2015-09-16 17:10:56 +0300

    update version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'lauri.jesmin@cashongo.co.uk'
 license          'All rights reserved'
 description      'Change security settings, add users'
 long_description 'Change security settings, add users'
-version          '0.2.5'
+version          '0.3.0'
 
 depends 'sudo',      '~> 2.7.1'
